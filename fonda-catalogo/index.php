26c0576a47e536e4cd748bef101e613626dd9032
francisco-q <quevedof98@gmail.com>
2025-08-27 03:35:05 -0400

    enrutado assets

--- fonda-catalogo/index.php
+++ fonda-catalogo/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fonda Juanita - Catálogo de Productos</title>
-    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
@@ -47,7 +47,7 @@
         </div>
     </div>
 
-    <script src="../assets/js/app.js"></script>
+    <script src="assets/js/app.js"></script>
 </body>
 
 </html>
